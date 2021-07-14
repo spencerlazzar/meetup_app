baec8b6609e07626392b9be18da9aaf3bdb4982b
Spencer Lazzar <spencerlazzar@yahoo.com>
2021-07-13 17:36:32 -0700

    Rending cards with props.children

--- src/components/meetups/MeetupItem.js
+++ src/components/meetups/MeetupItem.js
@@ -1,20 +1,23 @@
 import React from 'react';
 import classes from './MeetupItem.module.css';
+import Card from '../ui/Card';
 
 const MeetupItem = ({ meetup }) => {
   return (
     <li className={classes.item}>
-      <div className={classes.image}>
-        <img src={meetup.image} alt={meetup.title} />
-      </div>
-      <div className={classes.content}>
-        <h3>{meetup.title}</h3>
-        <address>{meetup.address}</address>
-        <p>{meetup.description}</p>
-      </div>
-      <div className={classes.actions}>
-        <button>To Favorites</button>
-      </div>
+      <Card>
+        <div className={classes.image}>
+          <img src={meetup.image} alt={meetup.title} />
+        </div>
+        <div className={classes.content}>
+          <h3>{meetup.title}</h3>
+          <address>{meetup.address}</address>
+          <p>{meetup.description}</p>
+        </div>
+        <div className={classes.actions}>
+          <button>To Favorites</button>
+        </div>
+      </Card>
     </li>
   );
 };
