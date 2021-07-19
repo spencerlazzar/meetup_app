f55a3462f6e6978301be60ef20882a6e381666e8
Spencer Lazzar <spencerlazzar@yahoo.com>
2021-07-19 15:54:55 -0700

    Navigating Programmatically

--- src/pages/NewMeetup.js
+++ src/pages/NewMeetup.js
@@ -1,11 +1,28 @@
+import { useHistory } from 'react-router-dom';
 import React from 'react';
 import NewMeetupForm from '../components/meetups/NewMeetupForm';
 
 const NewMeetupPage = () => {
+  const history = useHistory();
+
+  const addMeetupHanlder = (meetupData) => {
+    fetch(
+      'https://react-meetups-b4cd6-default-rtdb.firebaseio.com/meetups.json',
+      {
+        method: 'POST',
+        body: JSON.stringify(meetupData),
+        headers: {
+          'Content-type': 'application/json',
+        },
+      }
+    ).then(() => {
+      history.replace('/');
+    });
+  };
   return (
     <section>
       <h1>Add New Meetup</h1>
-      <NewMeetupForm />
+      <NewMeetupForm onAddMeetup={addMeetupHanlder} />
     </section>
   );
 };
